7297b9fe0436f77df04c0daf45a41ab2d6f697fb
Austin Culter <austin.culter@gusto.com>
2018-02-09 01:36:55 -0700

    Added rough, working paginator helper method.

--- lib/salapi/client.rb
+++ lib/salapi/client.rb
@@ -13,15 +13,31 @@ module SalAPI
         { 'publickey' => @pub_key.to_s, 'privatekey' => @priv_key.to_s }
     end
 
+    def paginator(request_url, total_pages)
+      i = 1
+      complete = []
+      while i <= total_pages
+        pg = HTTParty.get("#{request_url}/?page=#{i}", headers: @sal_headers)
+        pg["results"].each do |h|
+          complete << h
+        end
+        i += 1
+      end
+      complete
+    end
+
     # Returns an array of serials
     def machine_list
       url = "#{@sal_url}/api/machines"
       response = HTTParty.get(url, headers: @sal_headers)
-      if JSON.parse(response.body)["next"].nil? == false
-        paginator(url) # TODO: Broken. Build pagination funct. using count and ?page=
+      json = JSON.parse(response.body)
+      pages = (json['count'].to_f / 100.0).ceil
+      if json["next"].nil? == false
+        rtr = paginator(url, pages).map { |h| h["serial"] }
       else
-        JSON.parse(response.body)
+        rtr = json
       end
+      rtr
     end
 
     def machine_info(serial)
@@ -33,15 +49,27 @@ module SalAPI
     def machine_facts(serial)
       url = "#{@sal_url}/api/facts/#{serial}"
       response = HTTParty.get(url, headers: @sal_headers)
-      JSON.parse(response.body)
+      json = JSON.parse(response.body)
+      pages = (json['count'].to_f / 100.0).ceil
+      if json["next"].nil? == false
+        rtr = paginator(url, pages)
+      else
+        rtr = json
+      end
+      rtr
     end
 
     def machine_conditions(serial)
       url = "#{@sal_url}/api/conditions/#{serial}"
       response = HTTParty.get(url, headers: @sal_headers)
-      more = JSON.parse(response.body)["next"].nil?
-      puts more
-      #JSON.parse(response.body)
+      json = JSON.parse(response.body)
+      pages = (json['count'].to_f / 100.0).ceil
+      if json["next"].nil? == false
+        rtr = paginator(url, pages)
+      else
+        rtr = json
+      end
+      rtr
     end
 
     def machine_apps(serial)
@@ -59,13 +87,27 @@ module SalAPI
     def apps_list
       url = "#{@sal_url}/api/inventory"
       response = HTTParty.get(url, headers: @sal_headers)
-      JSON.parse(response.body)
+      json = JSON.parse(response.body)
+      pages = (json['count'].to_f / 100.0).ceil
+      if json["next"].nil? == false
+        rtr = paginator(url, pages)
+      else
+        rtr = json
+      end
+      rtr
     end
 
     def search(query)
       url = "#{@sal_url}/api/search/?query=#{query}"
       response = HTTParty.get(url, headers: @sal_headers)
-      JSON.parse(response.body)
+      json = JSON.parse(response.body)
+      pages = (json['count'].to_f / 100.0).ceil
+      if json["next"].nil? == false
+        rtr = paginator(url, pages)
+      else
+        rtr = json
+      end
+      rtr
     end
   end
 end
